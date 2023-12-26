2174506c18ee3bcef86354788bcff5325a196b40
Adekalu <141879538+Adekalu@users.noreply.github.com>
2023-12-26 18:10:55 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,110 @@
+/*var  a=2;
+var  b=3;
+var  c=a+b;
+document.write(c)
+
+var a=2;
+var b=3;
+var c=a-b;
+document.write(c)
+
+var a=2;
+var b=3;
+var c=a+b;
+document.write(c + "<br>")
+
+
+var a=2;
+var b=3;
+var c=ab;
+document.write(c)
+
+var a=2;
+var b=3;
+var c=a/b;
+document.write(c)
+
+
+var a=true;
+var b=false;
+var c=a&&b;
+document.write(c +"<br>");
+
+var a=true;
+var b=false;
+var c=a||b;
+document.write(c +"<br>");
+
+
+var a=true;
+var b=false;
+var c=(a&b);
+document.write(c +"<br>");
+
+
+var a=true;
+var b=false;
+var c=(a|b);
+document.write(c +"<br>");
+
+var a=true;
+var b=false;
+var c=~(a|b);
+document.write(c +"<br>");
+
+var a=true;
+var b=false;
+var c=(a^b);
+document.write(c +"<br>");
+
+var a=33
+var b=10
+var c=(a=b);
+document.write(c +"<br>")
+
+var a=33
+var b=10
+var c=(a+b);
+document.write(c +"<br>")
+
+var a=33
+var b=10
+var c=(a-b);
+document.write(c +"<br>")
+
+var a=33
+var b=10
+var c=(a*b);
+document.write(c +"<br>")
+
+var a=33
+var b=10
+var c=(a/b);
+document.write(c +"<br>")
+
+var a=33
+var b=10
+var c=(a%b);
+document.write(c +"<br>")
+
+var p=1000
+var r=2.5
+var t=36;
+var sp=(p*r*t/100)
+document.write(sp);
+
+
+var h=45
+var b=70
+var a=(1/2*b*h)
+document.write(a);*/
+
+var r=4
+var b=3.142
+var a=b*(r*r)
+document.write(a);
+
+
+
+
+
